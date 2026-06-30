184ad60f0bf4271dde0ebc577d26217d92169f8d
KaashiRaju <kaashiraju03@gmail.com>
2026-06-30 12:16:20 +0530

    Initialize Spring Boot project

---
+++ moviehub-backend/src/test/java/com/moviehub/moviehub_backend/MoviehubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviehub.moviehub_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviehubBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
